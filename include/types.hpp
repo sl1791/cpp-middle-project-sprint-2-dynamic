ceabb154e44429c4fc1cb4a4065386e490e296df
Anikin Alex <Anikin.AS2@spacecorp.ru>
2026-04-29 15:53:54 +0300

    add: explicit scan_result

--- include/types.hpp
+++ include/types.hpp
@@ -17,6 +17,10 @@ template <typename... Ts>
 struct scan_result {
     std::tuple<Ts...> values;
 
+    scan_result() = default;
+
+    explicit scan_result(std::tuple<Ts...> tuple) : values(std::move(tuple)) {}
+
     scan_result(Ts... args) : values(std::move(args)...) {}
 };
 
